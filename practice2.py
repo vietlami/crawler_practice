a8f8ce1626af1cc21064870b4b5d812fdf9b295e
virus <yystopf@163.com>
2018-08-16 18:41:58 +0800

    fix

--- practice2.py
+++ practice2.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
-# @Time : 2018/8/15 12:06 
-# @Author : virus 
-# @File : practice2.py 
-# @Desp : python
+# @Time : 2018/8/15 11:38
+# @Author : virus
+# @File : practice1.py
+# @Desp : 新浪新闻历史栏目
+
+import requests
+import numpy
+from lxml import etree
+
+
+url="http://blog.sina.com.cn/lm/history/"
+page=requests.get(url)
+html=page.content
+selector = etree.HTML(html)
+
+buyer=selector.xpath('//div[@class="bd"]//a/text()')
+
+print (buyer)
